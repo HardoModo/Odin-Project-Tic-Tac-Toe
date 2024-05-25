cb2adb5b98b971d132bfa90e39f8d1282388ef46
Shervorn <nrovrehs@yahoo.com>
2024-05-24 23:14:44 -0400

    Game master checks for start conditions

--- tictactoe.js
+++ tictactoe.js
@@ -1,12 +1,12 @@
 console.log("Connected")
 
-function createPlayer (name, symbol) {
+function createPlayer (name, symbol, playerid) {
   
     let score = 0;
     const getScore = () => score;
     const giveScore = () => score++;
   
-    return { name, symbol, getScore, giveScore };
+    return { name, symbol, playerid, getScore, giveScore };
 }
 
 const gameMaster = (function () {
@@ -14,18 +14,31 @@ const gameMaster = (function () {
     let playerArray = []
 
     const collectPlayerInfo = (playerid) =>
-        {
-            const playerName = document.getElementById(playerid+"name").value
-            const playerSymbol = document.getElementById(playerid+"symbol").value
+    {
+        const playerName = document.getElementById(playerid+"name").value
+        const playerSymbol = document.getElementById(playerid+"symbol").value
 
-            if (playerName == "" || playerSymbol == "") {
-                window.alert("Missing player info.")
-            } else {
-                playerArray.push(createPlayer(playerName, playerSymbol))
-                document.getElementById(playerid+"form").style.visibility = "hidden"
-                console.log(playerArray)
-                document.getElementById(playerid+"side").innerHTML = "Test"
+        if (playerName == "" || playerSymbol == "") {
+            window.alert("Missing player info.")
+        } else {
+            const player = createPlayer(playerName, playerSymbol, playerid)
+            playerArray.push(player)
+            document.getElementById(playerid+"form").style.visibility = "hidden"
+            document.getElementById(playerid+"side").innerHTML = 
+            "Player: " + player.name + "<br/><br/>" +
+            "Symbol: " + player.symbol + "<br/><br/>" +
+            "Score: " + player.getScore();
+
+            if (playerArray.length == 2) {
+                gameMaster.startGame()
             }
-        };
-    return { collectPlayerInfo };
+        }
+    };
+
+    const startGame = () =>
+    {
+        console.log(playerArray)
+    }
+
+    return { collectPlayerInfo, startGame };
   })();
