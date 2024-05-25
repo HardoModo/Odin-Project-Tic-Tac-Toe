1f75b8a6929b897ac7e00bf52480597ed64e6a72
Shervorn <nrovrehs@yahoo.com>
2024-05-24 22:22:00 -0400

    Create game master object

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,31 @@
 console.log("Connected")
+
+function createPlayer (name, symbol) {
+  
+    let score = 0;
+    const getScore = () => score;
+    const giveScore = () => score++;
+  
+    return { name, symbol, getScore, giveScore };
+}
+
+const gameMaster = (function () {
+    let gameArray = []
+    let playerArray = []
+
+    const collectPlayerInfo = (playerid) =>
+        {
+            const playerName = document.getElementById(playerid+"name").value
+            const playerSymbol = document.getElementById(playerid+"symbol").value
+
+            if (playerName == "" || playerSymbol == "") {
+                window.alert("Missing player info.")
+            } else {
+                playerArray.push(createPlayer(playerName, playerSymbol))
+                document.getElementById(playerid+"form").style.visibility = "hidden"
+                console.log(playerArray)
+                document.getElementById(playerid+"side").innerHTML = "Test"
+            }
+        };
+    return { collectPlayerInfo };
+  })();
